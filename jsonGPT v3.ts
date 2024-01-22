6d060c73a7eec383dfb5247a1a0797d6a1e57e1b
Jayyhk <jemallen10@gmail.com>
2024-01-22 14:17:38 -0500

    prettier formatting

--- jsonGPT v3.ts
+++ jsonGPT v3.ts
@@ -1,14 +1,14 @@
 import { Configuration, OpenAIApi } from "openai";
- 
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
- 
+
 interface OutputFormat {
   [key: string]: string | string[] | OutputFormat;
 }
- 
+
 export async function json_gpt(
   system_prompt: string,
   user_prompt: string | string[],
@@ -26,31 +26,31 @@ export async function json_gpt(
   const dynamic_elements: boolean = /<.*?>/.test(JSON.stringify(output_format));
   // if the output format contains list elements of [ or ], then we add to the prompt to handle lists
   const list_output: boolean = /\[.*?\]/.test(JSON.stringify(output_format));
- 
+
   // start off with no error message
   let error_msg: string = "";
- 
+
   for (let i = 0; i < num_tries; i++) {
     let output_format_prompt: string = `\nYou are to output ${
       list_output && "an array of objects in"
     } the following in json format: ${JSON.stringify(
       output_format
     )}. \nDo not put quotation marks or escape character \\ in the output fields.`;
- 
+
     if (list_output) {
       output_format_prompt += `\nIf output field is a list, classify output into the best element of the list.`;
     }
- 
+
     // if output_format contains dynamic elements, process it accordingly
     if (dynamic_elements) {
       output_format_prompt += `\nAny text enclosed by < and > indicates you must generate content to replace it. Example input: Go to <location>, Example output: Go to the garden\nAny output key containing < and > indicates you must generate the key name to replace it. Example input: {'<location>': 'description of location'}, Example output: {school: a place for education}`;
     }
- 
+
     // if input is in a list format, ask it to generate json in a list
     if (list_input) {
       output_format_prompt += `\nGenerate an array of json, one json for each input element.`;
     }
- 
+
     // Use OpenAI to get a response
     const response = await openai.createChatCompletion({
       temperature: temperature,
@@ -63,13 +63,13 @@ export async function json_gpt(
         { role: "user", content: user_prompt.toString() },
       ],
     });
- 
+
     let res: string =
       response.data.choices[0].message?.content?.replace(/'/g, '"') ?? "";
- 
+
     // ensure that we don't replace away apostrophes in text
     res = res.replace(/(\w)"(\w)/g, "$1'$2");
- 
+
     if (verbose) {
       console.log(
         "System prompt:",
@@ -78,11 +78,11 @@ export async function json_gpt(
       console.log("\nUser prompt:", user_prompt);
       console.log("\nGPT response:", res);
     }
- 
+
     // try-catch block to ensure output format is adhered to
     try {
       let output: any = JSON.parse(res);
- 
+
       if (list_input) {
         if (!Array.isArray(output)) {
           throw new Error("Output format not in an array of json");
@@ -90,7 +90,7 @@ export async function json_gpt(
       } else {
         output = [output];
       }
- 
+
       // check for each element in the output_list, the format is correctly adhered to
       for (let index = 0; index < output.length; index++) {
         for (const key in output_format) {
@@ -98,12 +98,12 @@ export async function json_gpt(
           if (/<.*?>/.test(key)) {
             continue;
           }
- 
+
           // if output field missing, raise an error
           if (!(key in output[index])) {
             throw new Error(`${key} not in json output`);
           }
- 
+
           // check that one of the choices given for the list of words is an unknown
           if (Array.isArray(output_format[key])) {
             const choices = output_format[key] as string[];
@@ -121,7 +121,7 @@ export async function json_gpt(
             }
           }
         }
- 
+
         // if we just want the values for the outputs
         if (output_value_only) {
           output[index] = Object.values(output[index]);
@@ -131,7 +131,7 @@ export async function json_gpt(
           }
         }
       }
- 
+
       return list_input ? output : output[0];
     } catch (e) {
       error_msg = `\n\nResult: ${res}\n\nError message: ${e}`;
@@ -139,6 +139,6 @@ export async function json_gpt(
       console.log("Current invalid json format ", res);
     }
   }
- 
+
   return [];
 }
--- jsonGPT v4.ts
+++ jsonGPT v4.ts
@@ -1,13 +1,13 @@
-import OpenAI from 'openai';
+import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
- 
+
 interface OutputFormat {
   [key: string]: string | string[] | OutputFormat;
 }
- 
+
 export async function json_gpt(
   system_prompt: string,
   user_prompt: string | string[],
@@ -25,31 +25,31 @@ export async function json_gpt(
   const dynamic_elements: boolean = /<.*?>/.test(JSON.stringify(output_format));
   // if the output format contains list elements of [ or ], then we add to the prompt to handle lists
   const list_output: boolean = /\[.*?\]/.test(JSON.stringify(output_format));
- 
+
   // start off with no error message
   let error_msg: string = "";
- 
+
   for (let i = 0; i < num_tries; i++) {
     let output_format_prompt: string = `\nYou are to output ${
       list_output && "an array of objects in"
     } the following in json format: ${JSON.stringify(
       output_format
     )}. \nDo not put quotation marks or escape character \\ in the output fields.`;
- 
+
     if (list_output) {
       output_format_prompt += `\nIf output field is a list, classify output into the best element of the list.`;
     }
- 
+
     // if output_format contains dynamic elements, process it accordingly
     if (dynamic_elements) {
       output_format_prompt += `\nAny text enclosed by < and > indicates you must generate content to replace it. Example input: Go to <location>, Example output: Go to the garden\nAny output key containing < and > indicates you must generate the key name to replace it. Example input: {'<location>': 'description of location'}, Example output: {school: a place for education}`;
     }
- 
+
     // if input is in a list format, ask it to generate json in a list
     if (list_input) {
       output_format_prompt += `\nGenerate an array of json, one json for each input element.`;
     }
- 
+
     // Use OpenAI to get a response
     const response = await openai.chat.completions.create({
       temperature: temperature,
@@ -62,13 +62,13 @@ export async function json_gpt(
         { role: "user", content: user_prompt.toString() },
       ],
     });
- 
+
     let res: string =
       response.choices[0].message?.content?.replace(/'/g, '"') ?? "";
- 
+
     // ensure that we don't replace away apostrophes in text
     res = res.replace(/(\w)"(\w)/g, "$1'$2");
- 
+
     if (verbose) {
       console.log(
         "System prompt:",
@@ -77,11 +77,11 @@ export async function json_gpt(
       console.log("\nUser prompt:", user_prompt);
       console.log("\nGPT response:", res);
     }
- 
+
     // try-catch block to ensure output format is adhered to
     try {
       let output: any = JSON.parse(res);
- 
+
       if (list_input) {
         if (!Array.isArray(output)) {
           throw new Error("Output format not in an array of json");
@@ -89,7 +89,7 @@ export async function json_gpt(
       } else {
         output = [output];
       }
- 
+
       // check for each element in the output_list, the format is correctly adhered to
       for (let index = 0; index < output.length; index++) {
         for (const key in output_format) {
@@ -97,12 +97,12 @@ export async function json_gpt(
           if (/<.*?>/.test(key)) {
             continue;
           }
- 
+
           // if output field missing, raise an error
           if (!(key in output[index])) {
             throw new Error(`${key} not in json output`);
           }
- 
+
           // check that one of the choices given for the list of words is an unknown
           if (Array.isArray(output_format[key])) {
             const choices = output_format[key] as string[];
@@ -120,7 +120,7 @@ export async function json_gpt(
             }
           }
         }
- 
+
         // if we just want the values for the outputs
         if (output_value_only) {
           output[index] = Object.values(output[index]);
@@ -130,7 +130,7 @@ export async function json_gpt(
           }
         }
       }
- 
+
       return list_input ? output : output[0];
     } catch (e) {
       error_msg = `\n\nResult: ${res}\n\nError message: ${e}`;
@@ -138,6 +138,6 @@ export async function json_gpt(
       console.log("Current invalid json format ", res);
     }
   }
- 
+
   return [];
 }
